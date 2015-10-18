9d46916e2dca76f419ca1a70f27a66dfbe76988b
Andrea Cremaschi <cremaschiandrea@gmail.com>
2015-10-18 21:29:02 +0200

    hack it with unsafeBitCasts!

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -5,7 +5,13 @@ import DummyCFramework
 typealias GEOSCallbackFunction = @convention(c) (UnsafeMutablePointer<CChar>) -> Void
 
 let swiftCallback : GEOSCallbackFunction = { args in
-    if let string = String.fromCString(args) {
+    let firstArg:UnsafeMutablePointer<CChar> = unsafeBitCast(args, UnsafeMutablePointer<CChar>.self)
+    let secondArg:UnsafeMutablePointer<CChar> = unsafeBitCast(args + Int(strlen(firstArg)) + 1, UnsafeMutablePointer<CChar>.self)
+    
+    if let string1 = String.fromCString(firstArg),
+        let string2 = String.fromCString(secondArg)
+    {
+        let string = String(format: string1, secondArg)
         print("GEOSwift # " + string + ".")
     }
 }
